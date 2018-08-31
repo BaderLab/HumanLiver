b74695181719ddb446aba639fe77b1006677a6a3
innesbre <brendan.innes@mail.utoronto.ca>
2018-08-31 11:20:24 -0400

    Added cluster annotations. v1.1.0

--- R/viewHumanLiver.R
+++ R/viewHumanLiver.R
@@ -20,7 +20,19 @@
 
 viewHumanLiver <- function(outPath="./") {
   filePath <- system.file("liver/HumanLiver.RData",package="HumanLiver")
-  cellMarkers <- list()
+  cellMarkers <- list("Hepatocytes"=c("ALB","HAMP","ARG1","PCK1","AFP","BCHE"),
+                      "LSECs"=c("CALCRL","CD32B","VWF"),
+                      "Cholangiocytes"=c("KRT19","EPCAM","FXDY2","CLDN4","CLDN10",
+                                       "SOX9","MMP7","CXCL1","CFTR","TFF2","KRT7","CD24"),
+                      "Hepatic Stellate Cells"=c("ACTA2","COL1A1","TAGLN","COL1A2",
+                                                 "COL3A1","SPARC","RBP1","DCN","MYL9"),
+                      "Macrophages"=c("CD68","MARCO"),
+                      "ab T cells"=c("CD2","CD3D","TRAC","IL32","CD3E"),
+                      "gd T cells"=c("NKG7","FCGR3A","HOPX","GNLY"),
+                      "NK cells"=c("GZMK","KLRF1","CCL3","CMC1"),
+                      "Plasma cells"=c("CD27","IGHG1"),
+                      "Mature B cells"=c("MS4A1","LTB","CD52","IGHD"),
+                      "Erythroid cells"=c("HBB","SLC25A37","CA1","ALAS2"))
 
   if (require("org.Hs.eg.db",quietly=T)) {
     annotationDB <- org.Hs.eg.db
